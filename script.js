495e509b36c95ba3ef174d87878addd4f19cccdf
Chandan Soni <sonichandansoni2001@gmail.com>
2025-01-15 00:11:20 -0500

    Added button hol logic

--- script.js
+++ script.js
@@ -9,17 +9,25 @@ const currentScore0El = document.getElementById('current--0');
 const currentScore1El = document.getElementById('current--1');
 
 const btnNew = document.querySelector('.btn--new');
-const btnOld = document.querySelector('.btn--old');
+const btnHold = document.querySelector('.btn--old');
 const btnRoll = document.querySelector('.btn--roll');
 
 diceEl.classList.add('hidden');
-
+let playing = true;
 const scores = [0,0];
 let currentScore = 0;
 let activePlayer = 0;
+const switchPlayer = function(){
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+}
 
 // 5 
 btnRoll.addEventListener('click',function(){
+    if(playing){
     // 1. generate a random dice roll
     const dice = Math.trunc(Math.random()*6) + 1;
 
@@ -36,13 +44,29 @@ btnRoll.addEventListener('click',function(){
     }
     else{
         // switch to the next player
-        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
-        currentScore = 0;
-        activePlayer = activePlayer === 0 ? 1 : 0;
-        player0El.classList.toggle('player--active');
-        player1El.classList.toggle('player--active');
-        
+        switchPlayer();
+    }
+}
+})
 
+
+btnHold.addEventListener('click',function(){
+    if(playing){
+    // 1. Add current score to active player's score
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+
+    // 2. Check if player's score >= 100
+    if(scores[activePlayer] >= 100){
+        // finish the game
+        playing = false;
+        document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+        document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
         
     }
+    else{
+    // Switch to the next player
+    switchPlayer();
+    }
+}
 })
